9c3787ba8a26e3a1a9cb7ccb9a94ab8ba0accdf5
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-22 19:24:27 +0500

    Some changes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let user_score = 0;
 let comuputer_socre = 0;
 
+const choise = document.querySelectorAll('.choise')
+
+
+const playcomputer = (user_choice) =>{
+
+}
+choise.forEach((choise) => {
+    console.log(choise)
+    choise.addEventListener('click', () =>{
+        const user_choice = choise.getAttribute('id');
+        // console.log("Choice was clicked" , choiceid);
+    })
+
+})
