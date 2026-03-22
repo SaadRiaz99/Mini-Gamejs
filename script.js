ec9c4ecd0dc4ba733815600bedafaa652d235a1c
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-22 19:33:28 +0500

    Add Calculation

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ let comuputer_socre = 0;
 const choise = document.querySelectorAll('.choise')
 
 
-const playcomputer = (user_choice) =>{
+const gencompchoice = () => {
+    const   choice = ["rock" , "paper" , "scossor"]
+    Math.floor(Math.random() * 10)
 
 }
+
+const playcomputer = (user_choice) =>{
+    console.log("user choice: " + user_choice);
+
+}
+
+
 choise.forEach((choise) => {
     console.log(choise)
     choise.addEventListener('click', () =>{
         const user_choice = choise.getAttribute('id');
         // console.log("Choice was clicked" , choiceid);
+        playcomputer(user_choice)
     })
 
 })
